3b07a2ee54a56c0bdd30c883f2eebd62407f2d98
Vaishnavi K <45496791+vaichu22@users.noreply.github.com>
2020-10-11 22:27:58 +0530

    Add files via upload

--- Arrays/PlusOne.java
+++ Arrays/PlusOne.java
@@ -3,36 +3,34 @@ package com.Arrays;
 public class PlusOne {
 	
 	public static int[] plusOne(int[] digits) {
-		//int n =0;
-		String s = "";
 		
-		for(int i=0; i<digits.length;i++) {
-			//n+=Integer.parseInt(Integer.toString(digits[i]));
-			s += Integer.toString(digits[i]);
-		}
-		int n = Integer.parseInt(s);
-		n+=1;
-		String ans = Integer.toString(n);
-		//System.out.println(ans);
+		int n = digits.length;
 		
-		int[] output = new int[ans.length()];
-		for(int i=0;i<ans.length();i++) {
-			//System.out.println(ans.charAt(i));
-			output[i] = Integer.parseInt(String.valueOf(ans.charAt(i)));
+		for(int i= n-1; i>=0; i--) {
+			if(digits[i] == 9) {
+				digits[i] =0;
+			}
+			else {
+				digits[i]++;
+				return digits;
+			}
 		}
-		//System.out.println(output[0]);
-		return output;
+		
+		digits = new int[n+1];
+		digits[0] = 1;
+		return digits;		
 		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] a = {9,8,7,6,5,4,3,2,1,0};
+		//int[] a = {9,8,7,6,5,4,3,2,1,0};
+		int[] a = {9,9,9};
 		int[] answer = plusOne(a);
 		
 		for(int i=0;i<answer.length;i++) {
-			System.out.println(answer[i]);
+			System.out.print(answer[i]);
 		}
 
 	}
